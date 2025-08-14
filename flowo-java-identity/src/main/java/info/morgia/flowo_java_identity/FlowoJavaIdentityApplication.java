d3704a0e1729e5744d1a6d883f4a7a66b84524b2
Emidio Morgia <emidio@morgia.info>
2025-08-14 17:49:10 +0200

    feat: initialize Flowo Java Identity microservice with Maven setup

---
+++ flowo-java-identity/src/main/java/info/morgia/flowo_java_identity/FlowoJavaIdentityApplication.java
@@ -0,0 +1,13 @@
+package info.morgia.flowo_java_identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowoJavaIdentityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowoJavaIdentityApplication.class, args);
+	}
+
+}
---
+++ flowo-java-identity/src/test/java/info/morgia/flowo_java_identity/FlowoJavaIdentityApplicationTests.java
@@ -0,0 +1,13 @@
+package info.morgia.flowo_java_identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowoJavaIdentityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
